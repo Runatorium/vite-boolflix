6bebda735a7a5e7014f670538824faa03b12accb
Runatorium <stefano.alesci1@gmail.com>
2023-03-31 17:19:07 +0200

    milestone 1 completata

--- src/components/MySearchedMovie.vue
+++ src/components/MySearchedMovie.vue
@@ -1,10 +1,5 @@
-<temeplate>
-
-
-</temeplate>
-
-
 <script>
+
 import { store } from '../store.js';
 
 export default{
@@ -17,9 +12,36 @@ export default{
           },
 }
 </script>
-    
+
+
+<template>
+    <div class="box">
+            <div v-for="(movie, index) in store.searchedMovie" class="movie">
+                <h1>{{movie.title}}</h1>
+                <h3>{{movie.original_title}}</h3>
+                <p>{{movie.original_language}}</p>
+                <h4>{{movie.vote_average}}</h4>
+            </div>
+    </div>
+</template>
+
+
 
 <style scoped>
+.box{
+    display: flex;
+    flex-wrap: wrap;
+    
+}
+.movie{
+    border: solid 2px black;
+    width: calc(100% / 6);
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    background-color: bisque;
+    
+}
 
 </style>
 
