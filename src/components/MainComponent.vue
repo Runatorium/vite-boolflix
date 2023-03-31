ee05569f2c88a90891b9072deff3c582cacff23c
Runatorium <stefano.alesci1@gmail.com>
2023-03-31 15:49:53 +0200

    milestone 0

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from '../store.js';
+
+
+export default{
+    name: 'MainComponent',
+
+    data() {
+            return {
+              store
+            }
+          },
+}
+
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
